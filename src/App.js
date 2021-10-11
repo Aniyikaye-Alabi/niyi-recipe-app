a14db692ddc28c02e53eaa380799af701f5dc9f7
Aniyikaye Alabi <adalabi@sidmach.com>
2021-10-11 11:24:27 +0100

    initial release

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Recipe from './Recipe';
 
 function App() {
+
+  const APP_ID = '187539f0';
+  const APP_KEY = '4c43ce4c5fa6a2e204e004b2b0efdb91';
+  //const sampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`;
+  
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
+
+  useEffect( () => {
+    getRequest();
+  }, [query]);
+
+  const getRequest = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    
+    const data = await response.json();
+    setRecipes(data.hits);
+    //console.log(data.hits);
+  }
+
+  console.log(recipes);
+
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  }
+  //console.log(search);
+
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getSearch} className='search-form'>
+        <input className='search-bar' type='text' value={search} onChange={updateSearch} />
+        <button className='search-button' type='submit'>Search</button>
+      </form>
+      <div className='recipes'>
+        {recipes.map(recipe => (
+            
+              <Recipe key={recipe.recipe.label} title={recipe.recipe.label} calories={recipe.recipe.calories} image_url={recipe.recipe.image} ingredients={recipe.recipe.ingredients} />
+          ))}
+        </div>
     </div>
   );
 }
